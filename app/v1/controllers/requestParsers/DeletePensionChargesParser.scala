7a2823772b2013165b3fe1bd5035c1b3049bfbdd
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2020-08-05 15:26:51 +0100

    added parser and test

---
+++ app/v1/controllers/requestParsers/DeletePensionChargesParser.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import javax.inject.Inject
+import uk.gov.hmrc.domain.Nino
+import v1.controllers.requestParsers.validators.Validator
+import v1.models.requestData.{DeletePensionChargesRawData, DeletePensionChargesRequest, DesTaxYear}
+
+//TODO REPLACE WITH ACTUAL VALIDATOR
+class DeletePensionChargesParser @Inject()(val validator: Validator[DeletePensionChargesRawData]) extends RequestParser[DeletePensionChargesRawData,
+  DeletePensionChargesRequest] {
+
+  override protected def requestFor(data: DeletePensionChargesRawData): DeletePensionChargesRequest =
+    DeletePensionChargesRequest(Nino(data.nino), DesTaxYear(data.taxYear))
+}
