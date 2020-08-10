5af23974132d53393589ebc1afeb6b77198241e9
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2020-08-10 10:24:29 +0100

    added json format to response model

--- app/v1/models/des/RetrievePensionChargesResponse.scala
+++ app/v1/models/des/RetrievePensionChargesResponse.scala
@@ -16,174 +16,80 @@
 
 package v1.models.des
 
-case class LumpSumBenefitTakenInExcessOfLifetimeAllowance(amount: BigDecimal, taxPaid: BigDecimal)
-
-case class BenefitInExcessOfLifetimeAllowance(amount: BigDecimal, taxPaid: BigDecimal)
+import play.api.libs.json.{Json, OFormat}
 
 case class LifetimeAllowance(amount: BigDecimal, taxPaid: BigDecimal)
 
-//scalastyle:off
-//pensionSavingsTaxCharges	Object	O
-//formats are the same and map to corresponding Retrieve Pension Charges Response model
-//pensionSavingsTaxCharges.pensionSchemeTaxReference	Array	M
-//pensionSavingsTaxCharges.lumpSumBenefitTakenInExcessOfLifetimeAllowance	Object O
-//pensionSavingsTaxCharges.lumpSumBenefitTakenInExcessOfLifetimeAllowance.amount	Number	M
-//pensionSavingsTaxCharges.lumpSumBenefitTakenInExcessOfLifetimeAllowance.taxPaid	Number	M
-//pensionSavingsTaxCharges.benefitInExcessOfLifetimeAllowance	Object	O
-//pensionSavingsTaxCharges.benefitInExcessOfLifetimeAllowance.amount Number	M
-//pensionSavingsTaxCharges.benefitInExcessOfLifetimeAllowance.taxPaid	Number	M
-//pensionSavingsTaxCharges.isAnnualAllowanceReduced	Boolean	O
-//pensionSavingsTaxCharges.taperedAnnualAllowance	Boolean	O
-//pensionSavingsTaxCharges.moneyPurchasedAllowance	Boolean	O
-//
+object LifetimeAllowance {
+  implicit val format: OFormat[LifetimeAllowance] = Json.format[LifetimeAllowance]
+}
+
 case class PensionSavingsTaxCharges(pensionSchemeTaxReference: Seq[String],
                                     lumpSumBenefitTakenInExcessOfLifetimeAllowance: Option[LifetimeAllowance],
                                     benefitInExcessOfLifetimeAllowance: Option[LifetimeAllowance],
                                     isAnnualAllowanceReduced: Option[Boolean],
                                     taperedAnnualAllowance: Option[Boolean],
-                                    moneyPurchasedAllowance: Option[Boolean]
-                                   )
+                                    moneyPurchasedAllowance: Option[Boolean])
+
+object PensionSavingsTaxCharges {
+  implicit val format: OFormat[PensionSavingsTaxCharges] = Json.format[PensionSavingsTaxCharges]
+}
 
 case class OverseasSchemeProvider(providerName: String,
                                   providerAddress: String,
                                   providerCountryCode: String,
-                                  qualifyingRecognisedOverseasPensionScheme: Seq[String]
-                                 )
+                                  qualifyingRecognisedOverseasPensionScheme: Seq[String])
+
+object OverseasSchemeProvider {
+  implicit val format: OFormat[OverseasSchemeProvider] = Json.format[OverseasSchemeProvider]
+}
 
 case class PensionSchemeOverseasTransfers(overseasSchemeProvider: Seq[OverseasSchemeProvider],
                                           transferCharge: BigDecimal,
-                                          transferChargeTaxPaid: BigDecimal
-                                         )
+                                          transferChargeTaxPaid: BigDecimal)
+
+object PensionSchemeOverseasTransfers {
+  implicit val format: OFormat[PensionSchemeOverseasTransfers] = Json.format[PensionSchemeOverseasTransfers]
+}
 
 case class Charge(amount: BigDecimal, foreignTaxPaid: BigDecimal)
 
+object Charge {
+  implicit val format: OFormat[Charge] = Json.format[Charge]
+}
+
 case class PensionSchemeUnauthorisedPayments(pensionSchemeTaxReference: Seq[String],
-                                             surcharge: Charge,
-                                             noSurcharge: Charge
-                                            )
+                                             surcharge: Option[Charge],
+                                             noSurcharge: Option[Charge])
+
+object PensionSchemeUnauthorisedPayments {
+  implicit val format: OFormat[PensionSchemeUnauthorisedPayments] = Json.format[PensionSchemeUnauthorisedPayments]
+}
 
 case class PensionContributions(pensionSchemeTaxReference: Seq[String],
                                 inExcessOfTheAnnualAllowance: BigDecimal,
-                                annualAllowanceTaxPaid: BigDecimal
-                               )
+                                annualAllowanceTaxPaid: BigDecimal)
 
-  case class OverseasPensionContributions(overseasSchemeProvider: OverseasPensionContributions,
-                                          shortServiceRefund: BigDecimal,
-                                          shortServiceRefundTaxPaid: BigDecimal
-                                         )
+object PensionContributions {
+  implicit val format: OFormat[PensionContributions] = Json.format[PensionContributions]
+}
+
+case class OverseasPensionContributions(overseasSchemeProvider: OverseasSchemeProvider,
+                                        shortServiceRefund: BigDecimal,
+                                        shortServiceRefundTaxPaid: BigDecimal)
+
+object OverseasPensionContributions {
+  implicit val format: OFormat[OverseasPensionContributions] = Json.format[OverseasPensionContributions]
+}
 
 case class RetrievePensionChargesResponse(pensionSavingsTaxCharges: Option[PensionSavingsTaxCharges],
-                                          pensionSchemeOverseasTransfers: PensionSchemeOverseasTransfers,
-                                          pensionSchemeUnauthorisedPayments: PensionSchemeUnauthorisedPayments,
-                                          pensionContributions: PensionContributions,
-                                          overseasPensionContributions: OverseasPensionContributions
-                                         ) {
+                                          pensionSchemeOverseasTransfers: Option[PensionSchemeOverseasTransfers],
+                                          pensionSchemeUnauthorisedPayments: Option[PensionSchemeUnauthorisedPayments],
+                                          pensionContributions: Option[PensionContributions],
+                                          overseasPensionContributions: Option[OverseasPensionContributions]) {
 
 }
 
-//scalastyle:off
-
-
-//pensionSchemeOverseasTransfers	Object	O
-//pensionSchemeOverseasTransfers.overseasSchemeProvider	Array	M
-//pensionSchemeOverseasTransfers.overseasSchemeProvider.providerName	String	M
-//pensionSchemeOverseasTransfers.overseasSchemeProvider.providerAddress	String	M
-//pensionSchemeOverseasTransfers.overseasSchemeProvider.providerCountryCode	String	M
-//pensionSchemeOverseasTransfers.overseasSchemeProvider.qualifyingRecognisedOverseasPensionScheme	Array(String)	M
-//pensionSchemeOverseasTransfers.transferCharge	Number	M
-//pensionSchemeOverseasTransfers.transferChargeTaxPaid	Number	M
-//
-//pensionSchemeUnauthorisedPayments	Object	O
-//pensionSchemeUnauthorisedPayments.pensionSchemeTaxReference	Array	M
-//pensionSchemeUnauthorisedPayments.surcharge	Object	O
-//pensionSchemeUnauthorisedPayments.surcharge.amount	Number	M
-//pensionSchemeUnauthorisedPayments.surcharge.foreignTaxPaid	Number	M
-//pensionSchemeUnauthorisedPayments.noSurcharge	Object	O
-//pensionSchemeUnauthorisedPayments.noSurcharge.amount	Number	M
-//pensionSchemeUnauthorisedPayments.noSurcharge.foreignTaxPaid	Number	M
-//
-//pensionContributions	Object	O
-//pensionContributions.pensionSchemeTaxReference	Array	M
-//pensionContributions.inExcessOfTheAnnualAllowance	Number	M
-//pensionContributions.annualAllowanceTaxPaid	Number	M
-//
-//overseasPensionContributions	Object	O
-//overseasPensionContributions.overseasSchemeProvider	Array	M
-//overseasPensionContributions.overseasSchemeProvider.providerName	String	M
-//overseasPensionContributions.overseasSchemeProvider.providerAddress	String	M
-//overseasPensionContributions.overseasSchemeProvider.providerCountryCode	String	M
-//overseasPensionContributions.overseasSchemeProvider.qualifyingRecognisedOverseasPensionScheme	Array	M
-//overseasPensionContributions.shortServiceRefund	Number	M
-//overseasPensionContributions.shortServiceRefundTaxPaid	Number	M
-
-//scalastyle:off
-//HTTP/1.1 200 OK
-//CorrelationId: a1e8057e-fbbc-47a8-a8b4-78d9f015c253
-//Content-Type: application/json
-//{
-//	"submittedOn": "2020-07-27T17:00:19Z",
-//	"pensionSavingsTaxCharges": {
-//		"pensionSchemeTaxReference": [
-//			"00123456RA"
-//		],
-//		"lumpSumBenefitTakenInExcessOfLifetimeAllowance": {
-//			"amount": 123.45,
-//			"taxPaid": 12.45
-//		},
-//		"benefitInExcessOfLifetimeAllowance": {
-//			"amount": 123.45,
-//			"taxPaid": 12.34
-//		},
-//		"isAnnualAllowanceReduced": true,
-//		"taperedAnnualAllowance": true,
-//		"moneyPurchasedAllowance": false
-//	},
-//	"pensionSchemeOverseasTransfers": {
-//		"overseasSchemeProvider": [
-//			{
-//				"providerName": "Overseas Pensions Plc",
-//				"providerAddress": "111 Main Street, George Town, Grand Cayman",
-//				"providerCountryCode": "ESP",
-//				"qualifyingRecognisedOverseasPensionScheme": [
-//					"Q123456"
-//				]
-//			}
-//		],
-//		"transferCharge": 123.45,
-//		"transferChargeTaxPaid": 0
-//	},
-//	"pensionSchemeUnauthorisedPayments": {
-//		"pensionSchemeTaxReference": [
-//			"00123456RA"
-//		],
-//		"surcharge": {
-//			"amount": 123.45,
-//			"foreignTaxPaid": 123.45
-//		},
-//		"noSurcharge": {
-//			"amount": 123.45,
-//			"foreignTaxPaid": 123.45
-//		}
-//	},
-//	"pensionContributions": {
-//		"pensionSchemeTaxReference": [
-//			"00123456RA"
-//		],
-//		"inExcessOfTheAnnualAllowance": 123.45,
-//		"annualAllowanceTaxPaid": 123.45
-//	},
-//	"overseasPensionContributions": {
-//		"overseasSchemeProvider": [
-//			{
-//				"providerName": "Overseas Pensions Plc",
-//				"providerAddress": "111 Main Street, George Town, Grand Cayman",
-//				"providerCountryCode": "ESP",
-//				"pensionSchemeTaxReference": [
-//					"00123456RA"
-//				]
-//			}
-//		],
-//		"shortServiceRefund": 123.45,
-//		"shortServiceRefundTaxPaid": 0
-//	}
-//}
+object RetrievePensionChargesResponse {
+  implicit val format: OFormat[RetrievePensionChargesResponse] = Json.format[RetrievePensionChargesResponse]
+}
