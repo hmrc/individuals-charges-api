414a261b311f2fe5ee89ca6fc899eff46f93dd73
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2020-08-13 16:17:12 +0100

    Drafted controller

--- app/v1/controllers/RetrievePensionChargesController.scala
+++ app/v1/controllers/RetrievePensionChargesController.scala
@@ -16,14 +16,46 @@
 
 package v1.controllers
 
+import cats.data.EitherT
 import javax.inject.{Inject, Singleton}
-import v1.services.{EnrolmentsAuthService, MtdIdLookupService}
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import v1.hateoas.HateoasFactory
+import v1.models.errors.ErrorWrapper
+import v1.models.requestData.{RetrievePensionChargesRawData, RetrievePensionChargesRequest}
+import v1.services.{AuditService, EnrolmentsAuthService, MtdIdLookupService, RetrievePensionChargesService}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class RetrievePensionChargesController @Inject()(val authService: EnrolmentsAuthService,
                                                 val lookupService: MtdIdLookupService,
-                                                retrievePensionChargesService)  {
+                                                retrievePensionChargesService: RetrievePensionChargesService,
+                                                retrievePensionsParser: ???,
+                                                hateoasFactory: HateoasFactory,
+                                                auditService: AuditService,
+                                                cc: ControllerComponents)(implicit ec: ExecutionContext) extends AuthorisedController(cc) with BaseController {
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "RetrievePensionChargesController", endpointName = "Retrieve a Pensions Charge")
+
+  def retrieve(nino: String, taxYear: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      val rawData = RetrievePensionChargesRawData(nino, taxYear)
+      val parsedRequest = EitherT.fromEither[Future](retrievePensionsChargesParser.parseRequest(rawData))
+      val serviceResponse : Future[RetrievePensionsOutcome] = parsedRequest match {
+        case Right(data) => service.deletePensionCharges(data)
+        case Left(errorWrapper) => Future.successful(Left(errorWrapper))
+      }
+    }
 
 
+
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    (errorWrapper.errors.head: @unchecked) match {
+      case BadRequestError | NinoFormatError | LossIdFormatError => BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError                                         => NotFound(Json.toJson(errorWrapper))
+      case DownstreamError                                       => InternalServerError(Json.toJson(errorWrapper))
+    }
+  }
 
 
 
