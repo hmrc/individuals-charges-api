fa643630f34219076f77608df8d3588a77082180
Jake Booker <45285591+jakebooker56@users.noreply.github.com>
2020-09-14 15:29:43 +0100

    changed coverage settings to 100

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -33,7 +33,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimum := 95,
+    ScoverageKeys.coverageMinimum := 100,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
