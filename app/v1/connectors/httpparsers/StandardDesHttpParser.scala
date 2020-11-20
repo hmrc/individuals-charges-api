570999c4fc2820c4a607cdbc6374b508912b1a2b
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2020-11-20 09:10:08 +0000

    minor amendment

--- app/v1/connectors/httpparsers/StandardDesHttpParser.scala
+++ app/v1/connectors/httpparsers/StandardDesHttpParser.scala
@@ -28,7 +28,7 @@ object StandardDesHttpParser extends HttpParser {
 
   case class SuccessCode(status: Int) extends AnyVal
 
-  val logger = Logger(getClass)
+  override val logger = Logger(getClass)
 
   // Return Right[DesResponse[Unit]] as success response has no body - no need to assign it a value
   implicit def readsEmpty(implicit successCode: SuccessCode = SuccessCode(NO_CONTENT)): HttpReads[DesOutcome[Unit]] =
