fa536699aa3807ec533f79e84ec6a38faae7d640
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-12-03 11:23:54 +0000

    MTDSA-7813 tweaking logging levels

--- app/v1/controllers/DeletePensionChargesController.scala
+++ app/v1/controllers/DeletePensionChargesController.scala
@@ -76,7 +76,7 @@ class DeletePensionChargesController @Inject()(val authService: EnrolmentsAuthSe
       result.leftMap { errorWrapper =>
         val resCorrelationId = errorWrapper.correlationId
         val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
-        logger.info(
+        logger.warn(
           s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
             s"Error response received with CorrelationId: $resCorrelationId")
 
--- app/v1/controllers/RetrievePensionChargesController.scala
+++ app/v1/controllers/RetrievePensionChargesController.scala
@@ -69,7 +69,7 @@ class RetrievePensionChargesController @Inject()(val authService: EnrolmentsAuth
       result.leftMap { errorWrapper =>
         val resCorrelationId = errorWrapper.correlationId
         val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
-        logger.info(
+        logger.warn(
           s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
             s"Error response received with CorrelationId: $resCorrelationId")
 
