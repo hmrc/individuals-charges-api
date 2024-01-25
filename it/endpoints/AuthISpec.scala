09645ce5772df79701c6d433a9f0ab7e7d9944e3
KobithasanVasantharajah <kobithasan.vasantharajah@uk.ey.com>
2024-01-25 15:52:15 +0000

    MTDSA-21741: Removed V1 version from the codebase and migrated DeletePensionCharges endpoint to V2

--- it/endpoints/AuthISpec.scala
+++ it/endpoints/AuthISpec.scala
@@ -41,7 +41,7 @@ class AuthISpec extends IntegrationBaseSpec {
       setupStubs()
       buildRequest(uri)
         .withHttpHeaders(
-          (ACCEPT, "application/vnd.hmrc.1.0+json"),
+          (ACCEPT, "application/vnd.hmrc.2.0+json"),
           (AUTHORIZATION, "Bearer 123") // some bearer token
         )
     }
---
+++ test/v2/controllers/DeletePensionChargesControllerSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import api.controllers.{ControllerBaseSpec, ControllerTestRunner}
+import api.models.audit.{AuditEvent, AuditResponse, GenericAuditDetail}
+import api.models.domain.{Nino, TaxYear}
+import api.models.errors.{ErrorWrapper, NinoFormatError, TaxYearFormatError}
+import api.models.outcomes.ResponseWrapper
+import api.services.{MockAuditService, MockEnrolmentsAuthService, MockMtdIdLookupService}
+import play.api.libs.json.JsValue
+import play.api.mvc.Result
+import v2.controllers.validators.MockDeletePensionChargesValidatorFactory
+import v2.mocks.services.MockDeletePensionChargesService
+import v2.models.request.deletePensionCharges.DeletePensionChargesRequestData
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class DeletePensionChargesControllerSpec
+    extends ControllerBaseSpec
+    with ControllerTestRunner
+    with MockEnrolmentsAuthService
+    with MockMtdIdLookupService
+    with MockDeletePensionChargesValidatorFactory
+    with MockDeletePensionChargesService
+    with MockAuditService {
+
+  private val taxYear     = "2020-21"
+  private val requestData = DeletePensionChargesRequestData(Nino(nino), TaxYear.fromMtd(taxYear))
+
+  "delete" should {
+    "return a successful response with header X-CorrelationId and body" when {
+      "the request received is valid" in new Test {
+        willUseValidator(returningSuccess(requestData))
+
+        MockDeletePensionChargesService
+          .delete(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        runOkTestWithAudit(expectedStatus = NO_CONTENT)
+      }
+    }
+
+    "return the error as per spec" when {
+      "the parser validation fails" in new Test {
+        willUseValidator(returning(NinoFormatError))
+
+        runErrorTestWithAudit(NinoFormatError)
+      }
+
+      "the service returns an error" in new Test {
+        willUseValidator(returningSuccess(requestData))
+
+        MockDeletePensionChargesService
+          .delete(requestData)
+          .returns(Future.successful(Left(ErrorWrapper(correlationId, TaxYearFormatError))))
+
+        runErrorTestWithAudit(TaxYearFormatError)
+      }
+    }
+  }
+
+  class Test extends ControllerTest with AuditEventChecking {
+
+    val controller = new DeletePensionChargesController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      service = mockDeleteBFLossService,
+      validatorFactory = mockDeletePensionChargesValidatorFactory,
+      auditService = mockAuditService,
+      cc = cc,
+      idGenerator = mockIdGenerator
+    )
+
+    override protected def callController(): Future[Result] = controller.delete(nino, taxYear)(fakeRequest)
+
+    override protected def event(auditResponse: AuditResponse, maybeRequestBody: Option[JsValue]): AuditEvent[GenericAuditDetail] =
+      AuditEvent(
+        auditType = "DeletePensionsCharges",
+        transactionName = "delete-pensions-charges",
+        detail = GenericAuditDetail(
+          userType = "Individual",
+          agentReferenceNumber = None,
+          versionNumber = "2.0",
+          params = Map("nino" -> nino, "taxYear" -> taxYear),
+          maybeRequestBody,
+          correlationId,
+          auditResponse
+        )
+      )
+
+  }
+
+}
