afe08943d19ba0d3994062f7dbea9fdff8092827
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2025-02-07 10:35:09 +0000

    add v3

---
+++ test/v3/createAmend/def1/model/request/PensionSavingsTaxChargesSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3.createAmend.def1.model.request
+
+import play.api.libs.json.{JsValue, Json}
+import shared.utils.UnitSpec
+
+class PensionSavingsTaxChargesSpec extends UnitSpec {
+
+  val responseModel: PensionSavingsTaxCharges = PensionSavingsTaxCharges(
+    pensionSchemeTaxReference = Seq("00123456RA"),
+    lumpSumBenefitTakenInExcessOfLifetimeAllowance = Some(LifetimeAllowance(123.12, 123.12)),
+    benefitInExcessOfLifetimeAllowance = Some(LifetimeAllowance(123.12, 123.12))
+  )
+
+  val responseJson: JsValue = Json.parse("""
+      |{
+      |    "pensionSchemeTaxReference": ["00123456RA"],
+      |    "lumpSumBenefitTakenInExcessOfLifetimeAllowance": {
+      |        "amount":123.12,
+      |        "taxPaid":123.12
+      |    },
+      |    "benefitInExcessOfLifetimeAllowance": {
+      |        "amount":123.12,
+      |        "taxPaid":123.12
+      |    }
+      |}
+      |""".stripMargin)
+
+  "reads" when {
+    "passed valid JSON" should {
+      "return a valid model" in {
+        responseModel shouldBe responseJson.as[PensionSavingsTaxCharges]
+      }
+    }
+  }
+
+  "writes" when {
+    "passed valid model" should {
+      "return valid JSON" in {
+        Json.toJson(responseModel) shouldBe responseJson
+      }
+    }
+  }
+
+}
---
+++ test/v3/delete/DeletePensionChargesServiceSpec.scala
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3.delete
+
+import shared.controllers.EndpointLogContext
+import shared.models.domain.{Nino, TaxYear}
+import shared.models.errors.{
+  DownstreamErrorCode,
+  DownstreamErrors,
+  ErrorWrapper,
+  InternalError,
+  MtdError,
+  NinoFormatError,
+  NotFoundError,
+  OutboundError,
+  RuleTaxYearNotSupportedError,
+  TaxYearFormatError
+}
+import shared.models.outcomes.ResponseWrapper
+import shared.services.ServiceSpec
+import uk.gov.hmrc.http.HeaderCarrier
+import v3.delete.def1.request.Def1_DeletePensionChargesRequestData
+import v3.delete.model.request.DeletePensionChargesRequestData
+
+import scala.concurrent.Future
+
+class DeletePensionChargesServiceSpec extends ServiceSpec {
+
+  val nino: Nino       = Nino("AA123456A")
+  val taxYear: TaxYear = TaxYear.fromMtd("2020-21")
+
+  trait Test extends MockDeletePensionChargesConnector {
+    implicit val hc: HeaderCarrier              = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    lazy val service = new DeletePensionChargesService(mockDeletePensionChargesConnector)
+  }
+
+  lazy val request: DeletePensionChargesRequestData = Def1_DeletePensionChargesRequestData(nino, taxYear)
+
+  "Delete Pension Charges" should {
+    "return a Right" when {
+      "the connector call is successful" in new Test {
+        MockDeletePensionChargesConnector
+          .deletePensionCharges(request)
+          .returns(Future.successful(Right(ResponseWrapper("resultId", ()))))
+
+        await(service.deletePensionCharges(request)) shouldBe Right(ResponseWrapper("resultId", ()))
+      }
+    }
+
+    "return that wrapped error as-is" when {
+      "the connector returns an outbound error" in new Test {
+        val someError: MtdError                                = InternalError
+        val downstreamResponse: ResponseWrapper[OutboundError] = ResponseWrapper(correlationId, OutboundError(someError))
+        MockDeletePensionChargesConnector.deletePensionCharges(request).returns(Future.successful(Left(downstreamResponse)))
+
+        await(service.deletePensionCharges(request)) shouldBe Left(ErrorWrapper(correlationId, someError))
+      }
+    }
+
+    "service" when {
+      "a service call is successful" should {
+        "return a mapped result" in new Test {
+          MockDeletePensionChargesConnector
+            .deletePensionCharges(request)
+            .returns(Future.successful(Right(ResponseWrapper("resultId", ()))))
+
+          await(service.deletePensionCharges(request)) shouldBe Right(ResponseWrapper("resultId", ()))
+        }
+      }
+      "a service call is unsuccessful" should {
+        def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+          s"return ${error.code} error when $downstreamErrorCode error is returned from the connector" in new Test {
+
+            MockDeletePensionChargesConnector
+              .deletePensionCharges(request)
+              .returns(Future.successful(Left(ResponseWrapper("resultId", DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+
+            await(service.deletePensionCharges(request)) shouldBe Left(ErrorWrapper("resultId", error))
+          }
+
+        val errors = Seq(
+          "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+          "INVALID_TAX_YEAR"          -> TaxYearFormatError,
+          "NO_DATA_FOUND"             -> NotFoundError,
+          "INVALID_CORRELATIONID"     -> InternalError,
+          "SERVER_ERROR"              -> InternalError,
+          "SERVICE_UNAVAILABLE"       -> InternalError,
+          "UNEXPECTED_ERROR"          -> InternalError
+        )
+
+        val extraTysErrors = Map(
+          "INVALID_CORRELATION_ID" -> InternalError,
+          "NOT_FOUND"              -> NotFoundError,
+          "TAX_YEAR_NOT_SUPPORTED" -> RuleTaxYearNotSupportedError
+        )
+
+        (errors ++ extraTysErrors).foreach(args => (serviceError _).tupled(args))
+      }
+    }
+  }
+
+}
