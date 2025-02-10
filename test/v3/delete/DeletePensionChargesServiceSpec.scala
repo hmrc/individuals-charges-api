695d9abb59dbfd86fc9dd4e59051a1349435d322
Davou Jobbi <96893264+DavouJ@users.noreply.github.com>
2025-02-10 16:35:03 +0000

    Update DeletePensionChargesServiceSpec.scala
    Optimised import

--- test/v3/delete/DeletePensionChargesServiceSpec.scala
+++ test/v3/delete/DeletePensionChargesServiceSpec.scala
@@ -19,7 +19,7 @@ package v3.delete
 import common.errors.RuleOutsideAmendmentWindow
 import shared.controllers.EndpointLogContext
 import shared.models.domain.{Nino, TaxYear}
-import shared.models.errors.{DownstreamErrorCode, DownstreamErrors, ErrorWrapper, InternalError, MtdError, NinoFormatError, NotFoundError, OutboundError, RuleTaxYearNotSupportedError, TaxYearFormatError}
+import shared.models.errors._
 import shared.models.outcomes.ResponseWrapper
 import shared.services.ServiceSpec
 import uk.gov.hmrc.http.HeaderCarrier
