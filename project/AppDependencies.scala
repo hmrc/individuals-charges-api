16789bfb1a4dfba0675c0da9d9dca6757a4f03d7
JoNB7777 <60427113+JoNB7777@users.noreply.github.com>
2025-06-19 18:13:23 +0100

    [MTDSA-30381] Refactoring integration test structure

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-import play.core.PlayVersion
-import play.sbt.PlayImport.*
+//import play.core.PlayVersion
+//import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
   val bootstrapPlayVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = Seq(
-    ws,
+//    ws,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
     "com.chuusai"                  %% "shapeless"                 % "2.4.0-M1",
@@ -31,13 +31,18 @@ object AppDependencies {
     "com.github.jknack"             % "handlebars"                % "4.4.0"
   )
 
-  def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.11.0"           % scope,
-    "org.scalacheck"         %% "scalacheck"             % "1.18.1"             % scope,
-    "org.scalamock"          %% "scalamock"              % "7.3.2"              % scope,
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapPlayVersion % scope,
-    "com.github.fge"          % "json-schema-validator"  % "2.2.6"              % scope,
-    "io.swagger.parser.v3"    % "swagger-parser-v3"      % "2.1.29"             % scope
-  )
+  def test: Seq[sbt.ModuleID] = Seq(
+    "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.11.0",
+    "org.scalacheck"         %% "scalacheck"             % "1.18.1",
+    "org.scalamock"          %% "scalamock"              % "7.3.2",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapPlayVersion,
+    "com.github.fge"          % "json-schema-validator"  % "2.2.6",
+    "io.swagger.parser.v3"    % "swagger-parser-v3"      % "2.1.29"
+  ).map(_ % Test)
+
+  val itDependencies: Seq[ModuleID] = Seq(
+    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.29",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.19.1"
+  ).map(_ % Test)
 
 }
