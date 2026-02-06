1955d3920c0a187b9fb4b48875a954a043024b0c
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2026-02-06 17:00:00 +0000

    MTDSA-16677 Update ConfidenceLevel to CL250

--- test/shared/services/EnrolmentsAuthServiceSpec.scala
+++ test/shared/services/EnrolmentsAuthServiceSpec.scala
@@ -312,7 +312,7 @@ class EnrolmentsAuthServiceSpec extends ServiceSpec with MockSharedAppConfig {
         .anyNumberOfTimes()
         .returns(
           ConfidenceLevelConfig(
-            confidenceLevel = ConfidenceLevel.L200,
+            confidenceLevel = ConfidenceLevel.L250,
             definitionEnabled = true,
             authValidationEnabled = authValidationEnabled
           )
